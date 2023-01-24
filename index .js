c6eebc5b178c4e74bcc903fe2d03a9d446961b85
Nestan <nestankeneshova16@gmail.com>
2023-01-24 14:31:03 +0600

    created viriables with number

--- index .js
+++ index .js
@@ -8,3 +8,9 @@ let myFavoriteBook = "If Tomorrow Comes";
 let myFood = "Sushi";
 let myHobby = "Dance";
 let myAnimal = "Cat";
+
+let muBirthday = 16;
+let myClass = 11;
+let myAge = 17;
+let myHeight = 165;
+let myFootSize = 38;
