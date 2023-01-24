c7faa975bdf7f69ee13ec1a1220bd52c9c836a8c
Nestan <nestankeneshova16@gmail.com>
2023-01-24 15:03:57 +0600

    added 6viriales

--- index .js
+++ index .js
@@ -14,3 +14,28 @@ let myClass = 11;
 let myAge = 17;
 let myHeight = 165;
 let myFootSize = 38;
+
+let lessensIsStarted = true;
+let myLightOf = false;
+
+let example1 = 4 + 4;
+let example2 = 10 - 4;
+let example3 = 2 * 2;
+let example4 = 10 / 2;
+let example5 = 6 / 1;
+let example6 = 5 * 0;
+let example7 = 55 + 55;
+let example8 = 4 ** 6;
+let example9 = (4 + 4) * 2;
+let example10 = (5 * 5) / 2;
+let example11 = 17 % 2;
+let example12 = 14 ** (1 / 3);
+let example13 = 5.1 * 1.5;
+
+let example14 = 5 > 10;
+let example15 = 23 < 55;
+let example16 = 30 === 30;
+let example17 = 15 === 5;
+let example18 = 17 >= 12;
+let example19 = 38 <= 56;
+let example20 = 22 !== 1;
